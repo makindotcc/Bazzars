de3714ad40e8bc5da963a56ed70c81211d6d47cb
filippop1 <sheep3395@gmail.com>
2015-03-20 23:18:19 +0100

    Changed "getFriendlyName(Item)"

--- src/main/java/pl/filippop1/bazzars/Utils.java
+++ src/main/java/pl/filippop1/bazzars/Utils.java
@@ -16,65 +16,11 @@
 
 package pl.filippop1.bazzars;
 
-import java.lang.reflect.Method;
-import static org.apache.commons.lang.WordUtils.capitalizeFully;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 
 public class Utils {
-    public static String capitalize(String line) {
-        if (line != null) {
-            return line.length() > 1 ? line.substring(0, 1).toUpperCase() + line.substring(1).toLowerCase() : line.toUpperCase();
-        } else {
-            return "";
-        }
-    }
-
-    public static String getFriendlyName(Material material) {
-        return material == null ? "Air" : getFriendlyName(new ItemStack(material), false);
-    }
-
-    private static Class localeClass = null;
-    private static Class craftItemStackClass = null, nmsItemStackClass = null, nmsItemClass = null;
-    private static String OBC_PREFIX = Bukkit.getServer().getClass().getPackage().getName();
-    private static String NMS_PREFIX = OBC_PREFIX.replace("org.bukkit.craftbukkit", "net.minecraft.server");
-
-    public static String getFriendlyName(ItemStack itemStack, boolean checkDisplayName) {
-        if (itemStack.getType() == Material.SAND) {
-            return "Sand";
-        } else if (itemStack == null || itemStack.getType() == Material.AIR) return "Air";
-        try {
-            if (craftItemStackClass == null) craftItemStackClass = Class.forName(OBC_PREFIX + ".inventory.CraftItemStack");
-            Method nmsCopyMethod = craftItemStackClass.getMethod("asNMSCopy", ItemStack.class);
-
-            if (nmsItemStackClass == null) nmsItemStackClass = Class.forName(NMS_PREFIX + ".ItemStack");
-            Object nmsItemStack = nmsCopyMethod.invoke(null, itemStack);
-
-            Object itemName = null;
-            if (checkDisplayName) {
-                Method getNameMethod = nmsItemStackClass.getMethod("getName");
-                itemName = getNameMethod.invoke(nmsItemStack);
-            } else {
-                Method getItemMethod = nmsItemStackClass.getMethod("getItem");
-                Object nmsItem = getItemMethod.invoke(nmsItemStack);
-
-                if (nmsItemClass == null) nmsItemClass = Class.forName(NMS_PREFIX + ".Item");
-
-                Method getNameMethod = nmsItemClass.getMethod("getName");
-                Object localItemName = getNameMethod.invoke(nmsItem);
-
-                if (localeClass == null) localeClass = Class.forName(NMS_PREFIX + ".LocaleI18n");
-                Method getLocaleMethod = localeClass.getMethod("get", String.class);
-
-                Object localeString = localItemName == null ? "" : getLocaleMethod.invoke(null, localItemName);
-                itemName = ("" + getLocaleMethod.invoke(null, localeString.toString() + ".name")).trim();
-            }
-            return itemName != null ? itemName.toString() : capitalizeFully(itemStack.getType().name().replace("_", " ").toLowerCase());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return capitalizeFully(itemStack.getType().name().replace("_", " ").toLowerCase());
+    public static String getFriendlyName(Material itemStack) {
+        return itemStack.toString().replace("_", " ").toLowerCase();
     }
  
 }

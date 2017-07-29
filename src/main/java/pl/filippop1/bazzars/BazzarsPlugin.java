1ae6b8a316f1081587d6ed3b524487e92a4adc9c
Filip <filippop1@outlook.com>
2017-07-30 00:19:51 +0200

    Aktualizacja metrics oraz hologramow. Optymalizacja blokady ruchu gdy gracz ma stworzony bazar.

--- src/main/java/pl/filippop1/bazzars/BazzarsPlugin.java
+++ src/main/java/pl/filippop1/bazzars/BazzarsPlugin.java
@@ -16,12 +16,13 @@
 
 package pl.filippop1.bazzars;
 
+import org.bstats.bukkit.Metrics;
 import pl.filippop1.bazzars.listeners.PlayerJoinListener;
-import java.io.IOException;
+
+import java.util.concurrent.Callable;
 import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcstats.Metrics;
 import pl.filippop1.bazzars.api.Bazar;
 import pl.filippop1.bazzars.api.BazarManager;
 import pl.filippop1.bazzars.command.CommandExecutor;
@@ -29,17 +30,18 @@ import pl.filippop1.bazzars.listeners.InventoryClickListener;
 import pl.filippop1.bazzars.listeners.InventoryCloseListener;
 import pl.filippop1.bazzars.listeners.PlayerCommandPreprocessListener;
 import pl.filippop1.bazzars.listeners.PlayerInteractListener;
-import pl.filippop1.bazzars.listeners.PlayerMoveListener;
 import pl.filippop1.bazzars.listeners.PlayerQuitListener;
-import pl.filippop1.bazzars.listeners.WorldChangeListener;
+import pl.filippop1.bazzars.task.PlayerMoveTask;
+import pl.filippop1.bazzars.task.PluginUpdater;
 
 public class BazzarsPlugin extends JavaPlugin {
-    public static final String AUTHORS = "Dzikoysk (hologramy) & filippop1";
+    public static final String AUTHORS = "filippop1";
     public static final String SOURCE_URL = "https://github.com/Thefilippop1PL/Bazzars";
     public static final String UPDATER = "https://raw.githubusercontent.com/Thefilippop1PL/Bazzars/master/updater.txt";
     private static Configuration configuration;
     private static BazzarsPlugin instance;
     private static String version;
+    private Metrics metrics;
     
     @Override
     public void onEnable() {
@@ -50,25 +52,19 @@ public class BazzarsPlugin extends JavaPlugin {
         this.loadConfiguration();
         
         // Metrics
-        try {
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        
+        this.loadMetrics();
+
         // Commands and listeners
         this.getCommand("bazar").setExecutor(new CommandExecutor());
         CommandExecutor.get().registerDefaults();
         this.getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerMoveListener(), this);
         this.getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
         this.getServer().getPluginManager().registerEvents(new InventoryCloseListener(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerCommandPreprocessListener(), this);
         this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        this.getServer().getPluginManager().registerEvents(new WorldChangeListener(), this);
-        
+        this.getServer().getScheduler().runTaskTimer(this, new PlayerMoveTask(), 20L, 20L);
+
         this.getLogger().log(Level.INFO, "Bazzars zostal zaladowany");
         
         this.getServer().getScheduler().runTaskLaterAsynchronously(this, new PluginUpdater(), 20L);
@@ -77,22 +73,28 @@ public class BazzarsPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         for (Bazar bazar : BazarManager.getBazars()) {
-            if (bazar != null && bazar.isOpen() && configuration.isHologramEnabled()) {
-                try {
-                    bazar.getHologram().destroy();
-                } catch (Exception ex) {
-                    Logger.getLogger(BazzarsPlugin.class.getName()).log(Level.SEVERE, null, ex);
-                }
+            if (bazar.getHologram() != null && bazar.getHologram().isSpawned()) {
+                bazar.getHologram().despawn();
             }
         }
         this.getLogger().log(Level.INFO, "Bazzars zostal wylaczony");
+    }
+
+    private void loadMetrics() {
+        metrics = new Metrics(this);
+        metrics.addCustomChart(new Metrics.SingleLineChart("open_bazaars", new Callable<Integer>() {
+            @Override
+            public Integer call() {
+                return BazarManager.getBazars().size();
+            }
+        }));
     }
     
     private void loadConfiguration() {
         this.saveDefaultConfig();
         configuration = new Configuration(this.getConfig());
     }
-    
+
     public static Configuration getConfiguration() {
         return configuration;
     }
